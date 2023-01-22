c5651bb4a412c9b75ce3b229c2041e1abb1c990f
n.melihova <melihova.qa@gmail.com>
2023-01-22 18:34:59 +0200

    applied slice method

--- task_score.js
+++ task_score.js
@@ -5,6 +5,6 @@ let average = (intr + git + js) / 3; //средний балл за модули
 
 console.log(`Average points is ${average}`);
 
-let studentName = "Natalia Melihova"; //имя студента
+let studentName = "N.Melihova"; //имя студента
 
-console.log(`Average points of ${studentName} is ${average}.`);
+console.log(`Average points of ${studentName.slice(2, 3)}. is ${average}.`);
