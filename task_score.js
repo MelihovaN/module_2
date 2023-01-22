f2d92981a46e9c55a3823fdc273f69898a691516
n.melihova <melihova.qa@gmail.com>
2023-01-22 18:15:57 +0200

    average points

---
+++ task_score.js
@@ -0,0 +1,10 @@
+let intr = 30; //баллы за модуль введение
+let git = 55; //баллы модуль git
+let js = 70; //баллы за модуль JS
+let average = (intr + git + js) / 3; //средний балл за модули
+
+console.log(`Average points is ${average}`);
+
+let studentName = "Natalia Melihova"; //имя студента
+
+console.log(`Average points of ${studentName} is ${average}.`);
