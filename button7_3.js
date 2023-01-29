eb02dedcf3915e3bbee50ba6f88af2694383845b
MelihovaN <116183278+MelihovaN@users.noreply.github.com>
2023-01-29 17:43:00 +0200

    Home work 7.3 Button properties

---
+++ button7_3.js
@@ -0,0 +1,28 @@
+class button {
+  constructor(type, height, width, colour) {
+    this.type = type;
+    this.height = height;
+    this.width = width;
+    this.colour = colour;
+  }
+ 
+}
+let sendButton = new button("click", "10cm", "40cm", "green");
+
+console.log(sendButton);
+
+// class button {
+//   constructor(type, height, width, colour) {
+//     this.type = type;
+//     this.height = height;
+//     this.width = width;
+//     this.colour = colour;
+//   }
+//     onClick = function () {
+//         alert(this.type)
+//     }      
+    
+// }
+// let sendButton = new button("click", "10cm", "40cm", "green");
+
+// console.log(propButton);
