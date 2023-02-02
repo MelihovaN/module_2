c2b5fd75f87e39212f0b6d9f29cc3b3ec2a1cb19
MelihovaN <116183278+MelihovaN@users.noreply.github.com>
2023-02-02 13:25:20 +0200

    Create expenses8_1.js

---
+++ expenses8_1.js
@@ -0,0 +1,19 @@
+let expenses = [
+  {
+    yearlyExp: [2500, 2600, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390],
+  },
+  {
+    yearlyExp: [500, 1000, 90000, 80, 980, 10, 400, 3000, 250, 45000, 1200],
+  },
+  {
+    yearlyExp: [20, 200, 8900, 3300, 2300, 1009, 2000, 670, 900, 900, 7000],
+  },
+];
+
+expenses.forEach((exp) => {
+  exp.yearlyExp.forEach((value) => {
+      if (value > 1000) console.log(`расходы: ${value}`);
+      let totalScore = 0;
+     });
+    
+});
